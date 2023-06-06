ae3f5047f2b1201b20a198e0f3f37e4976f69e2e
sergiobagd <ssergioffx@gmail.com>
2023-06-06 10:11:02 +0500

    feat: #862jx66qg: fix the test to the ready state

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    ceoLogin: 'ceo@tourmalinecore.com',
+    ceoPassword: 'cEoPa$$wo1d',
+    defaultCommandTimeout: 30000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  env: { MAILSLURP_API_KEY: 'd4297d96dcba82391396d35f1816ea1a84d1bf2b76bafec682756611cf383d00' },
 });
